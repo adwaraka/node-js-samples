a33260f38d49c5b2ab7bf6ca658fa6638b47bd83
Arvind D <arvindd25@gmail.com>
2017-10-11 10:59:54 -0700

    formatting issues

--- deck-cards/card.js
+++ deck-cards/card.js
@@ -1,31 +1,31 @@
 const suit = ["Clubs", "Diamonds", "Hearts", "Spades"],
-     rank = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+    rank = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
 
 let cardPrototype = {
-	suit: "",
-	rank: ""
+    suit: "",
+    rank: ""
 }
 
 let deckPrototype = {
-	cards: [],
-	count: 0,
+    cards: [],
+    count: 0,
 
-	createDeck: function(){
-		suit.forEach((suitVal) => {
-			rank.forEach((rankVal) => {
-				let card = {};
-				Object.assign(card, cardPrototype);
-				card.suit = suitVal;
-				card.rank = rankVal;
-				this.cards.push(card);
-				this.count+=1;
-			});
-		});
-	},
-	
-	showDeck: function(){
-		this.cards.forEach((value) => {console.log(value)});
-	}
+    createDeck: function(){
+        suit.forEach((suitVal) => {
+            rank.forEach((rankVal) => {
+                let card = {};
+                Object.assign(card, cardPrototype);
+                card.suit = suitVal;
+                card.rank = rankVal;
+                this.cards.push(card);
+                this.count+=1;
+            });
+        });
+    },
+    
+    showDeck: function(){
+        this.cards.forEach((value) => {console.log(value)});
+    }
 }
 
 deck = Object.create(deckPrototype);
