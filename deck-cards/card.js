51abb80086a83879102c189ab59e3c524cf7dc25
Arvind D <arvindd25@gmail.com>
2017-10-11 11:24:55 -0700

    adding shuffle logic, probably expensive

--- deck-cards/card.js
+++ deck-cards/card.js
@@ -10,6 +10,18 @@ let deckPrototype = {
     cards: [],
     count: 0,
 
+	swapCards: function(val1, val2) {
+		let temp = this.cards[val1];
+		this.cards[val1] = this.cards[val2];
+		this.cards[val2] = temp;
+	},
+
+    getRandomInt: function (min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;
+    },
+
     createDeck: function(){
         suit.forEach((suitVal) => {
             rank.forEach((rankVal) => {
@@ -22,13 +34,26 @@ let deckPrototype = {
             });
         });
     },
-    
+
     showDeck: function(){
         this.cards.forEach((value) => {console.log(value)});
-    }
+    },
+
+	shuffleDeck: function(count){
+		let val1, val2;
+		while (count > 0) {
+            val1 = this.getRandomInt(0, 52);
+		    val2 = this.getRandomInt(0, 52);
+            //console.log("swapping", val1, val2);
+            this.swapCards(val1, val2);
+			count-=1
+		}
+	}
 }
 
 deck = Object.create(deckPrototype);
 deck.createDeck();
+//deck.showDeck();
+//console.log(deck.count);
+deck.shuffleDeck(100);
 deck.showDeck();
-console.log(deck.count);
