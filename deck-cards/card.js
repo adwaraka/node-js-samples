dd49389bbd404d9e692f4ed71b45f78514a0a435
Arvind D <arvindd25@gmail.com>
2017-10-11 10:57:47 -0700

    adding code for a deck of cards

---
+++ deck-cards/card.js
@@ -0,0 +1,34 @@
+const suit = ["Clubs", "Diamonds", "Hearts", "Spades"],
+     rank = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+
+let cardPrototype = {
+	suit: "",
+	rank: ""
+}
+
+let deckPrototype = {
+	cards: [],
+	count: 0,
+
+	createDeck: function(){
+		suit.forEach((suitVal) => {
+			rank.forEach((rankVal) => {
+				let card = {};
+				Object.assign(card, cardPrototype);
+				card.suit = suitVal;
+				card.rank = rankVal;
+				this.cards.push(card);
+				this.count+=1;
+			});
+		});
+	},
+	
+	showDeck: function(){
+		this.cards.forEach((value) => {console.log(value)});
+	}
+}
+
+deck = Object.create(deckPrototype);
+deck.createDeck();
+deck.showDeck();
+console.log(deck.count);
